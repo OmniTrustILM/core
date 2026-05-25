20b0749ee130a3b07ac9ede0d314180618b047ad
Ivo Raisr <ivosh@ivosh.net>
2026-05-25 13:12:44 +0200

    Remove unused imports

--- src/test/java/com/czertainly/core/util/SpringBootTestContext.java
+++ src/test/java/com/czertainly/core/util/SpringBootTestContext.java
@@ -1,13 +1,11 @@
 package com.czertainly.core.util;
 
-import com.czertainly.core.security.authz.opa.OpaClient;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.task.SyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.client.ReactorResourceFactory;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.Duration;
 
