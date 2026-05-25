60ae8794997af5e4b328ff385a23cfbf8a6c2b0f
lubomirw <76479559+lubomirw@users.noreply.github.com>
2026-05-25 23:24:06 +0200

    Add CertificateStatusPollMessage + StatusPollProperties + config

---
+++ src/main/java/com/czertainly/core/messaging/model/CertificateStatusPollMessage.java
@@ -0,0 +1,13 @@
+package com.czertainly.core.messaging.model;
+
+import com.czertainly.api.model.core.auth.Resource;
+import com.czertainly.core.service.handler.authority.CertificateOperation;
+
+import java.util.UUID;
+
+public record CertificateStatusPollMessage(
+        Resource resourceType,
+        UUID resourceUuid,
+        CertificateOperation op,
+        int attempt
+) {}
