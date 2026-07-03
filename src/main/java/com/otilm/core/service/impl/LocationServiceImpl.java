a2aef5608fe21d1a76c8ade1ee9be93440c00a80
Ivo Raisr <ivosh@ivosh.net>
2026-07-03 08:57:32 +0200

    Replace @ExternalAuthorizationMissing markers with permanent authorization annotations (#1714)

--- src/main/java/com/otilm/core/service/impl/LocationServiceImpl.java
+++ src/main/java/com/otilm/core/service/impl/LocationServiceImpl.java
@@ -42,7 +42,6 @@ import com.otilm.core.enums.FilterField;
 import com.otilm.core.events.transaction.CertificateValidationEvent;
 import com.otilm.core.model.auth.ResourceAction;
 import com.otilm.core.security.authz.ExternalAuthorization;
-import com.otilm.core.security.authz.ExternalAuthorizationMissing;
 import com.otilm.core.security.authz.SecuredParentUUID;
 import com.otilm.core.security.authz.SecuredUUID;
 import com.otilm.core.security.authz.SecurityFilter;
@@ -1159,7 +1158,7 @@ public class LocationServiceImpl implements LocationExternalService, LocationInt
     }
 
     @Override
-    @ExternalAuthorizationMissing
+    @ExternalAuthorization(resource = Resource.LOCATION, action = ResourceAction.LIST)
     public List<SearchFieldDataByGroupDto> getSearchableFieldInformationByGroup() {
         final List<SearchFieldDataByGroupDto> searchFieldDataByGroupDtos = attributeEngine.getResourceSearchableFields(Resource.LOCATION, false);
 
