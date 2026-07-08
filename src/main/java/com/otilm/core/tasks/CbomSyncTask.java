e810355b1df4e361ad5de4530bcf86958233cac6
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-07-08 12:21:59 +0200

    Use REQUIRES_NEW for CbomSyncTask transaction

--- src/main/java/com/otilm/core/tasks/CbomSyncTask.java
+++ src/main/java/com/otilm/core/tasks/CbomSyncTask.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
@@ -50,7 +51,7 @@ public class CbomSyncTask implements ScheduledJobTask {
     }
 
     @Override
-    @Transactional(noRollbackFor = ScheduledJobSkippedException.class)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public ScheduledTaskResult performJob(final ScheduledJobInfo scheduledJobInfo, final Object taskData) {
         if (!cbomService.isCbomRepositoryClientConfigured()) {
             throw new ScheduledJobSkippedException();
