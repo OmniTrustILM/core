77336338a7e1c23a6a05d3c25eafafc77a0cc570
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-09-17 09:30:13 +0200

    Register the authorization decision caches
    Signed-off-by: klaraf755 <80590912+klaraf755@users.noreply.github.com>

--- src/main/java/com/otilm/core/config/cache/CacheConfig.java
+++ src/main/java/com/otilm/core/config/cache/CacheConfig.java
@@ -17,6 +17,7 @@ import org.springframework.core.Ordered;
 @EnableCaching(order = Ordered.HIGHEST_PRECEDENCE)
 @EnableConfigurationProperties({
         AuthCacheProperties.class,
+        AuthorizationCacheProperties.class,
         ConnectorApiClientCacheProperties.class,
         CertificateChainCacheProperties.class,
         CryptographicKeyItemCacheProperties.class,
@@ -31,6 +32,9 @@ public class CacheConfig {
     public static final String CONNECTOR_API_CLIENT_CACHE = "connectorApiClient";
     public static final String CREDENTIAL_VERIFICATION_CACHE = "credentialVerification";
     public static final String CRYPTOGRAPHIC_KEY_ITEM_CACHE = "cryptographicKeyItem";
+    public static final String OBJECT_AUTHZ_CACHE = "objectAuthz";
+    public static final String PRINCIPAL_DIGEST_CACHE = "principalDigest";
+    public static final String RESOURCE_AUTHZ_CACHE = "resourceAuthz";
     public static final String SIGNING_CERTIFICATE_CACHE = "signingCertificate";
     public static final String SIGNING_PROFILE_CACHE = "signingProfile";
     public static final String SYSTEM_USER_AUTH_CACHE = "systemUserAuth";
@@ -41,6 +45,7 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager(AuthCacheProperties authCacheProperties,
+            AuthorizationCacheProperties authorizationCacheProperties,
             CertificateChainCacheProperties certChainProperties,
             ConnectorApiClientCacheProperties connectorCacheProperties,
             CryptographicKeyItemCacheProperties cryptographicKeyItemCacheProperties, SecretRefIndex secretRefIndex,
@@ -107,6 +112,33 @@ public class CacheConfig {
                                 .newBuilder()
                                 .expireAfterWrite(cryptographicKeyItemCacheProperties.ttlMinutes(), TimeUnit.MINUTES)
                                 .maximumSize(cryptographicKeyItemCacheProperties.maxSize())
+                                .recordStats()
+                                .build());
+
+        mgr
+                .registerCustomCache(RESOURCE_AUTHZ_CACHE,
+                        Caffeine
+                                .newBuilder()
+                                .expireAfterWrite(authorizationCacheProperties.ttlMinutes(), TimeUnit.MINUTES)
+                                .maximumSize(authorizationCacheProperties.resourceMaxSize())
+                                .recordStats()
+                                .build());
+
+        mgr
+                .registerCustomCache(OBJECT_AUTHZ_CACHE,
+                        Caffeine
+                                .newBuilder()
+                                .expireAfterWrite(authorizationCacheProperties.ttlMinutes(), TimeUnit.MINUTES)
+                                .maximumSize(authorizationCacheProperties.objectMaxSize())
+                                .recordStats()
+                                .build());
+
+        mgr
+                .registerCustomCache(PRINCIPAL_DIGEST_CACHE,
+                        Caffeine
+                                .newBuilder()
+                                .expireAfterWrite(authorizationCacheProperties.ttlMinutes(), TimeUnit.MINUTES)
+                                .maximumSize(authorizationCacheProperties.principalDigestMaxSize())
                                 .recordStats()
                                 .build());
 
